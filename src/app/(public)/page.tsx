7633d17a43f3ab92e81cd43f5a9b73d1f9b5c6ec
Gabriel Martins <gab.smartinn@outlook.com>
2026-09-02 15:17:27 -0300

    feat: build public landing page

--- src/app/(public)/page.tsx
+++ src/app/(public)/page.tsx
@@ -1,5 +1,18 @@
+import { Clinics } from './_components/clinics'
+import { Footer } from './_components/footer'
 import { Header } from './_components/header'
+import { Hero } from './_components/hero'
 
 export default function Home() {
-    return <Header />
+    return (
+        <div className="flex flex-col min-h-screen">
+            <Header />
+
+            <div>
+                <Hero />
+                <Clinics />
+                <Footer />
+            </div>
+        </div>
+    )
 }
