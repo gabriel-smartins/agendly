8e2abf91a93810bd66c0b8559359dc38954b44e5
Gabriel Martins <gab.smartinn@outlook.com>
2026-09-04 16:43:41 -0300

    feat: add update service action

---
+++ src/app/(panel)/dashboard/services/_actions/update-service.ts
@@ -0,0 +1,59 @@
+'use server'
+
+import { revalidatePath } from 'next/cache'
+import { z } from 'zod'
+import { auth } from '@/lib/auth'
+import prisma from '@/lib/prisma'
+
+const formSchema = z.object({
+    serviceId: z.string().min(1, { message: 'O id do serviço é obrigatório.' }),
+    name: z.string().min(1, { message: 'O nome do serviço é obrigatório.' }),
+    price: z.number().min(1, { message: 'Informe um valor para o serviço.' }),
+    duration: z.number(),
+})
+
+type FormSchema = z.infer<typeof formSchema>
+
+export async function updateService(formData: FormSchema) {
+    const session = await auth()
+
+    if (!session?.user?.id) {
+        return {
+            error: 'Falha ao atualizar serviço',
+        }
+    }
+
+    const schema = formSchema.safeParse(formData)
+
+    if (!schema.success) {
+        return {
+            error: schema.error.issues[0].message,
+        }
+    }
+
+    try {
+        await prisma.service.update({
+            where: {
+                id: formData.serviceId,
+                userId: session?.user?.id,
+            },
+            data: {
+                name: formData.name,
+                price: formData.duration,
+                duration: formData.duration < 30 ? 30 : formData.duration,
+            },
+        })
+
+        revalidatePath('/dashboard/services')
+
+        return {
+            data: 'Serviço atualizado com sucesso!',
+        }
+    } catch (error) {
+        console.error(error)
+
+        return {
+            error: 'Falha ao atualizar serviço',
+        }
+    }
+}
