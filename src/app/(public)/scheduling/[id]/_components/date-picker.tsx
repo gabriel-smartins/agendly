2a5d15e697af237f1d814e264b2d268f66bb1f12
Gabriel Martins <gab.smartinn@outlook.com>
2026-09-07 15:54:30 -0300

    feat: add date picker component

---
+++ src/app/(public)/scheduling/[id]/_components/date-picker.tsx
@@ -0,0 +1,40 @@
+import { ptBR } from 'date-fns/locale/pt-BR'
+import { useState } from 'react'
+import DatePicker, { registerLocale } from 'react-datepicker'
+import 'react-datepicker/dist/react-datepicker.css'
+
+registerLocale('pt-BR', ptBR)
+
+interface DateTimePickerProps {
+    minDate?: Date
+    className?: string
+    initialDate?: Date
+    onChange: (date: Date) => void
+}
+
+export function DateTimePicker({
+    className,
+    minDate,
+    initialDate,
+    onChange,
+}: DateTimePickerProps) {
+    const [startDate, setStartDate] = useState(initialDate || new Date())
+
+    function handleChange(date: Date | null) {
+        if (date) {
+            setStartDate(date)
+            onChange(date)
+        }
+    }
+
+    return (
+        <DatePicker
+            className={className}
+            selected={startDate}
+            locale="pt-BR"
+            minDate={minDate ?? new Date()}
+            onChange={handleChange}
+            dateFormat="dd/MM/yyyy"
+        />
+    )
+}
