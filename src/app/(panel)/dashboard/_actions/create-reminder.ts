ed8f3dd5ca8024c68f98787c7484c57f4dc54192
Gabriel Martins <gab.smartinn@outlook.com>
2026-09-07 17:43:21 -0300

    feat: add create reminder action

---
+++ src/app/(panel)/dashboard/_actions/create-reminder.ts
@@ -0,0 +1,53 @@
+'use server'
+
+import { revalidatePath } from 'next/cache'
+import { z } from 'zod'
+import { auth } from '@/lib/auth'
+import prisma from '@/lib/prisma'
+
+const formSchema = z.object({
+    description: z
+        .string()
+        .min(1, { message: 'A descrição do lembrete é obrigatória.' }),
+})
+
+type FormSchema = z.infer<typeof formSchema>
+
+export async function createReminder(formData: FormSchema) {
+    const session = await auth()
+
+    if (!session?.user?.id) {
+        return {
+            error: 'Falha ao cadastrar lembrete.',
+        }
+    }
+
+    const schema = formSchema.safeParse(formData)
+
+    if (!schema.success) {
+        return {
+            error: schema.error.issues[0].message,
+        }
+    }
+
+    try {
+        await prisma.reminder.create({
+            data: {
+                description: formData.description,
+                userId: session?.user?.id,
+            },
+        })
+
+        revalidatePath('/dashboard')
+
+        return {
+            data: 'Lembrete cadastrado com sucesso!',
+        }
+    } catch (error) {
+        console.error(error)
+
+        return {
+            error: 'Falha ao cadastrar lembrete.',
+        }
+    }
+}
