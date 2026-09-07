b9dd17611006d90464efd80fc9be8a8d253dcc18
Gabriel Martins <gab.smartinn@outlook.com>
2026-09-07 15:54:25 -0300

    feat: add create appointment action

---
+++ src/app/(public)/scheduling/[id]/_actions/create-appointment.ts
@@ -0,0 +1,56 @@
+'use server'
+
+import { z } from 'zod'
+import prisma from '@/lib/prisma'
+
+const formSchema = z.object({
+    name: z.string().min(1, { message: 'O nome é obrigatório.' }),
+    email: z.string().email({ message: 'O email é obrigatório.' }),
+    phone: z.string().min(1, { message: 'O telefone é obrigatório.' }),
+    date: z.date(),
+    serviceId: z.string().min(1, { message: 'O serviço é obrigatório.' }),
+    time: z.string().min(1, { message: 'O horário é obrigatório.' }),
+    clinicId: z.string().min(1, { message: 'O usuário é obrigatório.' }),
+})
+
+type FormSchema = z.infer<typeof formSchema>
+
+export async function createNewAppointment(formData: FormSchema) {
+    const schema = formSchema.safeParse(formData)
+
+    if (!schema.success) {
+        return {
+            error: schema.error.issues[0].message,
+        }
+    }
+
+    try {
+        const selectedDate = new Date(formData.date)
+        const year = selectedDate.getFullYear()
+        const month = selectedDate.getMonth()
+        const day = selectedDate.getDay()
+
+        const appointmentDate = new Date(year, month, day, 0, 0, 0, 0)
+        const newAppointment = await prisma.appointment.create({
+            data: {
+                name: formData.name,
+                email: formData.email,
+                phone: formData.phone,
+                time: formData.time,
+                appointementDate: appointmentDate,
+                serviceId: formData.serviceId,
+                userId: formData.clinicId,
+            },
+        })
+
+        return {
+            data: newAppointment,
+        }
+    } catch (error) {
+        console.error(error)
+
+        return {
+            error: 'Erro ao realizar agendamento.',
+        }
+    }
+}
