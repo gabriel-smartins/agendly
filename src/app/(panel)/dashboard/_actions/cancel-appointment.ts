9ddc3fd685a6be5a7e7e174369c32b3ec3ec8c09
Gabriel Martins <gab.smartinn@outlook.com>
2026-09-09 14:19:20 -0300

    feat: add cancel appointment action

---
+++ src/app/(panel)/dashboard/_actions/cancel-appointment.ts
@@ -0,0 +1,53 @@
+'use server'
+
+import { revalidatePath } from 'next/cache'
+import { z } from 'zod'
+import { auth } from '@/lib/auth'
+import prisma from '@/lib/prisma'
+
+const formSchema = z.object({
+    appointmentId: z
+        .string()
+        .min(1, { message: 'O id do agendamento é obrigatório.' }),
+})
+
+type FormSchema = z.infer<typeof formSchema>
+
+export async function cancelAppointment(formData: FormSchema) {
+    const session = await auth()
+
+    if (!session?.user?.id) {
+        return {
+            error: 'Falha ao cancelar agendamento.',
+        }
+    }
+
+    const schema = formSchema.safeParse(formData)
+
+    if (!schema.success) {
+        return {
+            error: schema.error.issues[0].message,
+        }
+    }
+
+    try {
+        await prisma.appointment.delete({
+            where: {
+                id: formData.appointmentId,
+                userId: session?.user?.id,
+            },
+        })
+
+        revalidatePath('/dashboard')
+
+        return {
+            data: 'Agendamendo cancelado com sucesso!',
+        }
+    } catch (error) {
+        console.error(error)
+
+        return {
+            error: 'Falha ao cancelar agendamento.',
+        }
+    }
+}
