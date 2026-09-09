2d82e57a025c78079274297e40378b1e7ec0702c
Gabriel Martins <gab.smartinn@outlook.com>
2026-09-09 14:19:13 -0300

    feat: render registered clinics dynamically on home page

--- src/app/(public)/page.tsx
+++ src/app/(public)/page.tsx
@@ -2,15 +2,20 @@ import { Clinics } from './_components/clinics'
 import { Footer } from './_components/footer'
 import { Header } from './_components/header'
 import { Hero } from './_components/hero'
+import { getProfessionals } from './_data-access/get-professionals'
 
-export default function Home() {
+export const revalidate = 120
+
+export default async function Home() {
+    const professionals = await getProfessionals()
+
     return (
         <div className="flex flex-col min-h-screen">
             <Header />
 
             <div>
                 <Hero />
-                <Clinics />
+                <Clinics professionals={professionals || []} />
                 <Footer />
             </div>
         </div>
