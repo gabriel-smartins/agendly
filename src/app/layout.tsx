48db4ad80bf5f289490bbe9ba92f65796b3ede44
Gabriel Martins <gab.smartinn@outlook.com>
2026-09-09 14:19:01 -0300

    feat: wire up react query provider in root layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import { Geist, Geist_Mono } from 'next/font/google'
 import './globals.css'
 import { Toaster } from 'sonner'
 import SessionAuthProvider from '@/components/session-auth'
+import { QueryClientContext } from '@/providers/query-client'
 
 const geistSans = Geist({
     variable: '--font-geist-sans',
@@ -30,8 +31,10 @@ export default function RootLayout({
                 className={`${geistSans.variable} ${geistMono.variable} antialiased`}
             >
                 <SessionAuthProvider>
-                    <Toaster duration={2500} />
-                    {children}
+                    <QueryClientContext>
+                        <Toaster duration={2500} />
+                        {children}
+                    </QueryClientContext>
                 </SessionAuthProvider>
             </body>
         </html>
